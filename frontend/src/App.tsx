015c30622f70bd165ed4edd5ef825fe549aa9f07
Elena Balakireva <balakireva.elena@pronit.ru>
2025-05-19 16:08:06 +0300

    Шапка и футер

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,28 +2,37 @@ import './App.css'
 import About from './components/About/About'
 import Cart from './components/Cart/Cart'
 import Catalog from './components/Catalog/Catalog'
+import Contacts from './components/Contacts/Contacts'
 import ErrorPage from './components/ErrorPage/ErrorPage'
+import Header from './components/Header/Header'
 import Item from './components/Item/Item'
 import MainPage from './components/MainPage/MainPage'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './components/Footer/Footer'
 
 function App() {
 
   return (
-    <BrowserRouter>
-    <div>
-      <div className="page">
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/catalog.html" element={<Catalog />} />
-          <Route path="/about.html" element={<About />} />
-          <Route path=" /catalog/:id.html" element={<Item />} />
-          <Route path="/cart.html" element={<Cart />} />
-          <Route path="/404.html" element={<ErrorPage />} />
-        </Routes>
-      </div>
-    </div>
-  </BrowserRouter>
+    <>
+    <Header/>
+      <BrowserRouter>
+        <div>
+          <div className="page">
+            <Routes>
+              <Route path="/" element={<MainPage />} />
+              <Route path="/catalog.html" element={<Catalog />} />
+              <Route path="/about.html" element={<About />} />
+              <Route path=" /catalog/:id.html" element={<Item />} />
+              <Route path="/cart.html" element={<Cart />} />
+              <Route path="/404.html" element={<ErrorPage />} />
+              <Route path="/contacts.html" element={<Contacts />} />
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
+      <Footer/>
+    </>
   )
 }
 
